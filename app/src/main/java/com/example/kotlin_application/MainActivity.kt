82fd94e911a032b09c5eb3e726ce91a957b43884
CodeAlpacat <magicmars123@gmail.com>
2021-11-05 15:25:56 +0900

    commit_finished

--- app/src/main/java/com/example/kotlin_application/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_application/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.kotlin_application
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +18,28 @@ class MainActivity : AppCompatActivity() {
         val weightEditText = findViewById<EditText>(R.id.weightEditText)
 
         val resultButton: Button = findViewById(R.id.resultButton)
+
+        resultButton.setOnClickListener{
+
+
+            if(heightEditText.text.isEmpty() || weightEditText.text.isEmpty()){
+                Toast.makeText(this, "빈 값을 채워주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            //return 문으로 실행을 종료. 이후의 연산이 실행되지 않음.
+            //값을 넣지 않으면 실행이 되지 않음.
+
+            val height:Int = heightEditText.text.toString().toInt()
+            val weight:Int = weightEditText.text.toString().toInt()
+            //height와 weight를 Int형으로 선언하기위해서 text를 문자열로, 문자열에서 Int로 변환해줌.
+
+            val intent = Intent(this, ResultActivity::class.java)
+
+            intent.putExtra("height", height)
+            intent.putExtra("weight", weight)
+
+            startActivity(intent)
+        }
 
 
     }
